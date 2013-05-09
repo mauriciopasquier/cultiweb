5d3eb3bc36409e95800b4ddf72da9dcf46051c74
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-05-09 14:32:38 -0300

    Inicial con algunos cambios desde la instalación

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'middleman'
+
+gem 'sass-rails'
+gem 'bootstrap-sass'
---
+++ config.rb
@@ -0,0 +1,22 @@
+require 'bootstrap-sass'
+
+#compass_config do |config|
+#  config.http_path = '/'
+#  config.images_dir = 'img'
+#  config.javascripts_dir = 'js'
+#end
+
+set :css_dir, 'css'
+set :js_dir, 'js'
+set :images_dir, 'img'
+
+# pretty urls
+activate :directory_indexes
+
+activate :asset_hash
+
+configure :build do
+  # minificar!
+  activate :minify_css
+  activate :minify_javascript
+end
